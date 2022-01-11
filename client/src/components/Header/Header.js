34bf08648bf148cc5ef2715715d8975ea9a9a9cd
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-10 21:27:15 -0800

    added additional logic for active button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,25 +2,28 @@ import instockImg from '/Users/brynfrayne/Desktop/instock/client/src/assets/logo
 import React from 'react';
 import "./Header.scss";
 import { useLocation } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
  
  export default function Header() {
-     //assigning location variable
-     const location = useLocation();
+    //  //assigning location variable
+    //  const location = useLocation();
 
-     //destructuring pathname from location
-     const { pathname } = location;
+    //  //destructuring pathname from location
+    //  const { pathname } = location;
  
-     //Javascript split method to get the name of the path in array
-     const splitLocation = pathname.split("/"); 
+    //  //Javascript split method to get the name of the path in array
+    //  const splitLocation = pathname.split("/"); 
      
      return (
          <div className="header">
              <img src={instockImg} alt=''/>
              <div className='header__button-container'>
-                 <button className={`header__button ${splitLocation[1] === "Warehouses" ? "active-button" : ""}`}>Warehouses</button>
-                 <button className={`header__button ${splitLocation[1] === "Inventory" ? "active-button" : ""}`}>Inventory</button>
+                 <NavLink to='/' className='header__button' >Warehouses</NavLink>
+                 <NavLink to='/' className='header__button'>Inventory</NavLink>
              </div>
          </div>
      )
  }
  
+//  <button className={`header__button ${splitLocation[1] === "Warehouses" ? "active-button" : ""}`}>Warehouses</button>
+//                  <button className={`header__button ${splitLocation[1] === "Inventory" ? "active-button" : ""}`}>Inventory</button>
