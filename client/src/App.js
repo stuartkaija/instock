bf65c0759aef7e508c7c7b41961f1ffdf227b371
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-10 21:38:10 -0800

    added basic footer styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+// import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-        <h1>instock</h1>
+      {/* <Header /> */}
+      <Footer />
     </div>
   );
 }
