2bf2b750cc4b6bec0f7b34e9b6dfc3ca3dce2956
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-12 17:51:05 -0500

    added styling and additional containers

--- client/src/components/ItemForm/ItemForm.jsx
+++ client/src/components/ItemForm/ItemForm.jsx
@@ -4,7 +4,7 @@ import "./ItemForm.scss";
 function ItemForm() {
     return (
         <form className="item-form">
-            <section className="item-from__section">
+            <section className="item-form__section">
                 <h3 className="item-form__title">Item Details</h3>
                 <label htmlFor="name" className="item-form__label">Item Name</label>
                 <input 
@@ -28,13 +28,32 @@ function ItemForm() {
                     <option value="Gear">Gear</option>
                 </select>
             </section>
-            <section className="item-from__section">
+            <section className="item-form__section">
             <h3 className="item-form__title">Item Availability</h3>
-                <p className="item-form__label">Status</p>
-                <input type="radio" id="InStock" name="InStock" value="InStock"/>
-                <label htmlFor="InStock">In stock</label>
-                <input type="radio" id="OutOfStock" name="OutOfStock" value="OutOfStock"/>
-                <label htmlFor="OutOfStock">Out of stock</label>
+                <div className="item-form__radio-section">
+                    <p className="item-form__label item-form__label--radio">Status</p>
+                    <div className="item-form__radio-wrapper">
+                        <input 
+                            type="radio" 
+                            id="InStock" 
+                            name="InStock" 
+                            value="InStock"
+                            className="item-form__radio-button"
+                        />
+                        <label htmlFor="InStock">In stock</label>
+                    </div>
+                    <div className="item-form__radio-wrapper">    
+                        <input 
+                            type="radio" 
+                            id="OutOfStock" 
+                            name="OutOfStock" 
+                            value="OutOfStock"
+                            className="item-form__radio-button"
+                        />
+                        <label htmlFor="OutOfStock">Out of stock</label>
+                    </div>
+                </div>
+                
                 <label htmlFor="quantity" className="item-form__label">Quantity</label>
                 <input 
                     type="number" 
@@ -51,7 +70,7 @@ function ItemForm() {
                     className="item-form__input" 
                     placeholder="PLACEHOLDER"
                 />
-                <button className="item-form__button--secondary">Cancel</button>
+                <button className="item-form__button-secondary">Cancel</button>
                 <button className="item-form__button">PLACEHOLDER</button>
             </section>
         </form>
