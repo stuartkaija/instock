20097e613eb987ea435394440b23e4ceea24a967
Ian Virtue <ianvirtue101@gmail.com>
2022-01-12 00:19:56 -0500

    styled inventory list for mobile

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -1,3 +1,4 @@
+import "./InventoryList.scss";
 import chevronRight from "../../assets/icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
@@ -5,32 +6,35 @@ import editIcon from "../../assets/icons/edit-24px.svg";
 function InventoryList() {
   return (
     <>
-      <h1>Inventory List</h1>
-      <div>
-        <div>Inventory Item</div>
-        <div>
-          <div>Television</div>
-          <img src={chevronRight} />
+      <section className="inventory-list">
+        <div className="inventory-container">
+          <div className="inventory-column">
+            <div className="inventory-column__item--title">Inventory Item</div>
+            <div className="inventory-column__item-container">
+              <div className="inventory-column__item--main">Television</div>
+              <img src={chevronRight} />
+            </div>
+
+            <div className="inventory-column__item--title">Category</div>
+            <div className="inventory-column__item">Electronics</div>
+          </div>
+          <div className="inventory-container__column">
+            <div className="inventory-column__item--title">Status</div>
+            <div className="inventory-column__item--in-stock">In Stock</div>
+
+            <div className="inventory-column__item--title">QTY</div>
+            <div className="inventory-column__item">500</div>
+
+            <div className="inventory-column__item--title">Warehouse</div>
+            <div className="inventory-column__item">Manhattan</div>
+          </div>
         </div>
-
-        <div>Category</div>
-        <div>Electronics</div>
-      </div>
-      <div>
-        <div>Status</div>
-        <div>In Stock</div>
-
-        <div>QTY</div>
-        <div>500</div>
-
-        <div>Warehouse</div>
-        <div>Manhatran</div>
-      </div>
-
-      <div>
-        <img src={deleteIcon} />
-        <img src={editIcon} />
-      </div>
+        <div className="inventory-buttons">
+          {/* // These will likely be switched to NavLinks  */}
+          <img className="inventory-buttons__delete" src={deleteIcon} />
+          <img className="inventory-buttons__edit" src={editIcon} />
+        </div>
+      </section>
     </>
   );
 }
