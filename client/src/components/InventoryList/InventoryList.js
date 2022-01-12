729dc9ccadd4778bda09cd407c08055d487319ec
Ian Virtue <ianvirtue101@gmail.com>
2022-01-11 21:02:13 -0500

    added basic layout for inventory list page

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -1,7 +1,36 @@
+import chevronRight from "../../assets/icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+
 function InventoryList() {
   return (
     <>
       <h1>Inventory List</h1>
+      <div>
+        <div>Inventory Item</div>
+        <div>
+          <div>Television</div>
+          <img src={chevronRight} />
+        </div>
+
+        <div>Category</div>
+        <div>Electronics</div>
+      </div>
+      <div>
+        <div>Status</div>
+        <div>In Stock</div>
+
+        <div>QTY</div>
+        <div>500</div>
+
+        <div>Warehouse</div>
+        <div>Manhatran</div>
+      </div>
+
+      <div>
+        <img src={deleteIcon} />
+        <img src={editIcon} />
+      </div>
     </>
   );
 }
