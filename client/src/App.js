bc510da4335bc54d50afce05100c6eb9f67bdf10
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-13 11:20:16 -0800

    added hardcoded warehouse details component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Component } from 'react'
 import EditInvItem from "./components/EditInvItem/EditInvItem";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 
 
 class App extends Component {
@@ -10,7 +11,8 @@ class App extends Component {
       <BrowserRouter>
        <Header />
         <Switch>
-          <Route path="/warehouses" />
+          <Route exact path="/warehouses" />
+          <Route path="/warehouses/warehouseId" component={WarehouseDetails} />
           <Route path="/inventory" />
         </Switch>
       </BrowserRouter>
