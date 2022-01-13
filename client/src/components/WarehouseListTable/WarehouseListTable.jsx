4a81b42eef88c088a53042c0f120053c7022e7d8
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-13 16:43:18 -0500

    added styling

--- client/src/components/WarehouseListTable/WarehouseListTable.jsx
+++ client/src/components/WarehouseListTable/WarehouseListTable.jsx
@@ -6,45 +6,43 @@ import editIcon from "../../assets/icons/edit-24px.svg";
 
 function WarehouseListTable() {
     return (
-        <div>
-            <section className="inventory-list">
-                <div className="inventory-container">
-                    <div className="inventory-column">
-                        <div className="inventory-column__item--title">Inventory Item</div>
-                        <div className="inventory-column__item-container">
-                        <div className="inventory-column__item--main">Television</div>
+            <section className="warehouse-list">
+                <div className="warehouse-container">
+                    <div className="warehouse-column">
+                        <div className="warehouse-column__item--title">Inventory Item</div>
+                        <div className="warehouse-column__item-container">
+                        <div className="warehouse-column__item--main">Television</div>
                         <img src={chevronRight} alt="chevron right" />
                         </div>
 
-                        <div className="inventory-column__item--title">Category</div>
-                        <div className="inventory-column__item">Electronics</div>
+                        <div className="warehouse-column__item--title">Category</div>
+                        <div className="warehouse-column__item">Electronics</div>
                     </div>
-                    <div className="inventory-column">
-                        <div className="inventory-column__item--title">Status</div>
-                        <div className="inventory-column__item--in-stock">In Stock</div>
+                    <div className="warehouse-column">
+                        <div className="warehouse-column__item--title">Status</div>
+                        <div className="warehouse-column__item--in-stock">In Stock</div>
 
-                        <div className="inventory-column__item--title">QTY</div>
-                        <div className="inventory-column__item">500</div>
+                        <div className="warehouse-column__item--title">QTY</div>
+                        <div className="warehouse-column__item">500</div>
 
-                        <div className="inventory-column__item--title">Warehouse</div>
-                        <div className="inventory-column__item">Manhattan</div>
+                        <div className="warehouse-column__item--title">Warehouse</div>
+                        <div className="warehouse-column__item">Manhattan</div>
                     </div>
-                    <div className="inventory-buttons">
+                    <div className="warehouse-buttons">
                         {/* // These will likely be switched to NavLinks  */}
                         <img
-                        className="inventory-buttons__delete"
+                        className="warehouse-buttons__delete"
                         src={deleteIcon}
                         alt="delete-icon"
                         />
                         <img
-                        className="inventory-buttons__edit"
+                        className="warehouse-buttons__edit"
                         src={editIcon}
                         alt="edit-icon"
                         />
                     </div>
                 </div>
             </section>
-        </div>
     )
 }
 
