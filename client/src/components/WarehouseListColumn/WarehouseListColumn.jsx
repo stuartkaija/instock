a5834468669f43d75446ff6ba57771c97cfa3eb7
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-13 21:09:41 -0500

    added more styling, but in flex hell

--- client/src/components/WarehouseListColumn/WarehouseListColumn.jsx
+++ client/src/components/WarehouseListColumn/WarehouseListColumn.jsx
@@ -7,46 +7,54 @@ import editIcon from "../../assets/icons/edit-24px.svg";
 function WarehouseListColumn({ name, address, city, country, contactName, contactPhone, contactEmail}) {
   return (
     <section className="warehouse-list">
-      <div className="warehouse-container">
-        <div className="warehouse-column">
-          <h4 className="warehouse-column__item-title">
-              Warehouse
-          </h4>
-          <div className="warehouse-column__item-container">
-            <h5 className="warehouse-column__item--main">
-                {name}
-            </h5>
-            <img src={chevronRight} alt="chevron right" />
+      <div className="warehouse-column__main-container">
+        <div className="warehouse-list__container">
+          <div className="warehouse-column-container">
+            <h4 className="warehouse-column__title">
+                Warehouse
+            </h4>
+            <div className="warehouse-column__item-container">
+              <h5 className="warehouse-column__text warehouse-column__text--link">
+                  {name}
+              </h5>
+              <img src={chevronRight} alt="chevron right" />
+            </div>
+
+            <h4 className="warehouse-column__title">
+              Address
+            </h4>
+            <address className="warehouse-column__text">
+              {address}, {city}, {country}
+            </address>
           </div>
 
-          <h5 className="warehouse-column__item-title">Address</h5>
-          <address className="warehouse-column__item">{address}, {city}, {country}</address>
-        </div>
-
-        <div className="warehouse-column">
-          <h4 className="warehouse-column__item-title">contact name</h4>
-          <h5 className="warehouse-column__item">{contactName}</h5>
-          <h4 className="warehouse-column__item-title">contact information</h4>
-          <a href={`tel:${contactPhone}`}>
-            {contactPhone}
-          </a>
-          <a href={`mailto:${contactEmail}`} className="warehouse-column__item" >
-          {contactEmail}
-          </a>
-        </div>
-        <div className="warehouse-buttons">
-          <img
-            className="warehouse-buttons__delete"
-            src={deleteIcon}
-            alt="delete-icon"
-          />
-          <img
-            className="warehouse-buttons__edit"
-            src={editIcon}
-            alt="edit-icon"
-          />
-        </div>
+          <div className="warehouse-column--secondary">
+            <h4 className="warehouse-column__title">contact name</h4>
+            <h5 className="warehouse-column__text">{contactName}</h5>
+            <h4 className="warehouse-column__title">contact information</h4>
+            <div className="warehouse-column__contact-container">
+              <a href={`tel:${contactPhone}`} className="warehouse-column__text warehouse-column__text-link" >
+                {contactPhone}
+              </a>
+              <a href={`mailto:${contactEmail}`} className="warehouse-column__text warehouse-column__text-link" >
+              {contactEmail}
+              </a>
+            </div>
+          </div>
       </div>
+          <div className="warehouse-buttons">
+            <img
+              className="warehouse-buttons__delete"
+              src={deleteIcon}
+              alt="delete-icon"
+            />
+            <img
+              className="warehouse-buttons__edit"
+              src={editIcon}
+              alt="edit-icon"
+            />
+          </div>
+        </div>
     </section>
   );
 }
