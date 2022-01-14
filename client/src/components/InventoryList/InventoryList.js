5054481778e2e5a3b11ea47d2487dadbc7c31663
Ian Virtue <ianvirtue101@gmail.com>
2022-01-14 14:00:31 -0500

    fixed issue with inentorylist returning undefined

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -13,61 +13,68 @@ function InventoryList({ inventories }) {
   return (
     <>
       <section className="inventory-list">
-        {inventories.map((inventory) => (
-          <div key={inventory.id} className="inventory-container">
-            <div className="inventory-column">
-              <div className="inventory-column__item--title">
-                Inventory Item
-              </div>
-              <div className="inventory-column__item-container">
-                <NavLink key={inventory.id} to={`/inventory/${inventory.id}`}>
-                  <div className="inventory-column__item--main">
-                    {inventory.itemName}
-                  </div>
-                </NavLink>
+        {inventories.map((inventory) => {
+          console.log(inventory);
+          return (
+            <div key={inventory.id} className="inventory-container">
+              <div className="inventory-column">
+                <div className="inventory-column__item--title">
+                  Inventory Item
+                </div>
+                <div className="inventory-column__item-container">
+                  <NavLink key={inventory.id} to={`/inventory/${inventory.id}`}>
+                    <div className="inventory-column__item--main">
+                      {inventory.itemName}
+                    </div>
+                  </NavLink>
 
-                <img src={chevronRight} alt="" />
+                  <img src={chevronRight} alt="" />
+                </div>
+
+                <div className="inventory-column__item--title">Category</div>
+                <div className="inventory-column__item">
+                  {inventory.category}
+                </div>
+              </div>
+              <div className="inventory-column">
+                <div className="inventory-column__item--title">Status</div>
+                <div
+                  className={
+                    inventory.status.includes("In")
+                      ? "inventory-column__item--in-stock"
+                      : "inventory-column__item--out-of-stock"
+                  }
+                >
+                  {inventory.status}
+                </div>
+
+                <div className="inventory-column__item--title">QTY</div>
+                <div className="inventory-column__item">
+                  {inventory.quantity}
+                </div>
+
+                <div className="inventory-column__item--title">Warehouse</div>
+                <div className={"inventory-column__item"}>
+                  {inventory.warehouseName}
+                </div>
               </div>
 
-              <div className="inventory-column__item--title">Category</div>
-              <div className="inventory-column__item">{inventory.category}</div>
+              <div className="inventory-buttons">
+                {/* // These will likely be switched to NavLinks  */}
+                <img
+                  className="inventory-buttons__delete"
+                  src={deleteIcon}
+                  alt="delete-icon"
+                />
+                <img
+                  className="inventory-buttons__edit"
+                  src={editIcon}
+                  alt="edit-icon"
+                />
+              </div>
             </div>
-            <div className="inventory-column">
-              <div className="inventory-column__item--title">Status</div>
-              <div
-              // className={
-              //   inventory.status.includes("In")
-              //     ? "inventory-column__item--in-stock"
-              //     : "inventory-column__item--out-of-stock"
-              // }
-              >
-                {inventory.status}
-              </div>
-
-              <div className="inventory-column__item--title">QTY</div>
-              <div className="inventory-column__item">{inventory.quantity}</div>
-
-              <div className="inventory-column__item--title">Warehouse</div>
-              <div className={"inventory-column__item"}>
-                {inventory.warehouseName}
-              </div>
-            </div>
-
-            <div className="inventory-buttons">
-              {/* // These will likely be switched to NavLinks  */}
-              <img
-                className="inventory-buttons__delete"
-                src={deleteIcon}
-                alt="delete-icon"
-              />
-              <img
-                className="inventory-buttons__edit"
-                src={editIcon}
-                alt="edit-icon"
-              />
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </section>
     </>
   );
