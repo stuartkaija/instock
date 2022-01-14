acbeebb26d977fe2c8e51b8292e1dfbab7853f8d
Ian Virtue <ianvirtue101@gmail.com>
2022-01-13 23:39:05 -0500

    created back end post request for new inventory list item

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -26,7 +26,21 @@ router.get("", (req, res) => {});
 router.post("/", (req, res) => {
   const newInventoryItem = {
     id: uniqid(),
+    // warehouseID: "2922c286-16cd-4d43-ab98-c79f698aeab0",
+    warehouseName: req.body.warehouseName,
+    itemName: req.body.itemName,
+    description: req.body.description,
+    category: req.body.category,
+    status: req.body.status,
+    quantity: req.body.quantity,
   };
+
+  const inventory = readInventories();
+  inventory.push(newInventoryItem);
+  fs.writeFileSync("./data/inventories.json", JSON.stringify(inventory));
+
+  // Respond with the Inventory that was created
+  res.status(201).json(newInventoryItem);
 });
 
 // PUT/PATCH/EDIT an inventory item (Enrique)
