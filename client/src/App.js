3e8fa0372b868fed09dab3ac0e954410b6d83577
Enrique Barco <enrique.barco96@hotmail.com>
2022-01-15 18:17:57 -0600

    Added final algo for category and warehouse

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import InventoryItemPage from "./pages/InventoryItemPage/InventoryItemPage";
 import SpecificWarehousePage from "./pages/SpecificWarehousePage/SpecificWarehousePage";
 import EditWarehouseComponent from "./components/EditWarehouseComponent/EditWarehouseComponent";
 import EditInvItemPage from "./pages/EditInvItemPage/EditInvItemPage";
-import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 
 class App extends Component {
   render() {
@@ -20,10 +19,10 @@ class App extends Component {
           <Route path="/warehouses" exact component={WarehousesPage} />
           <Route path="/warehouses/:warehouseId" exact component={SpecificWarehousePage} />
           <Route path="/warehouses/:warehouseId/edit" component={EditWarehouseComponent} />
+          <Route path="/inventory/:inventoryId/edit" component={EditInvItemPage} />
           <Route path="/inventory" component={Inventory} />
           <Route path="/inventory/AddNew" exact component={AddInventoryItem} />
           <Route path="/inventory" exact component={Inventory} />
-          <Route path="/inventory/:inventoryId/edit" component={EditInvItemPage} />
           <Route path="/inventory/:itemId" component={InventoryItemPage}/>
           <Route path="/warehouses/:warehouseId" component={SpecificWarehousePage} />
         </Switch>
