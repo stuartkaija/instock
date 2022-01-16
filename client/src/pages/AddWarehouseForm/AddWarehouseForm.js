c02efe75685b0f5b4c5ee085264568a0ad18a4e5
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-15 16:58:21 -0800

    added corresponding custom validation to figma desktop mockup

--- client/src/pages/AddWarehouseForm/AddWarehouseForm.js
+++ client/src/pages/AddWarehouseForm/AddWarehouseForm.js
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom';
 import { Component } from 'react';
 import uniqid from 'uniqid'; 
 import axios from 'axios';
+import error from "../../assets/icons/error-24px.svg";
 
 
 export default class AddWarehouseForm extends Component {
@@ -15,6 +16,21 @@ export default class AddWarehouseForm extends Component {
         const validRegexPhone = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
         
         if (!event.target.name.value || !event.target.address.value || !event.target.city.value || !event.target.country.value || !event.target.contactName.value ||!event.target.position.value || !event.target.phone.value || !event.target.email.value ){
+           const input = document.getElementsByTagName('input');
+            for (let i = 0; i < input.length; i++) {
+            input[i].style.borderColor = "red";
+            }
+        
+           //    console.log(input.name.classList);
+        //    const values = ['name', 'address', 'city', 'country', 'contactName', 'position', 'phone', 'email'];
+        const span = document.getElementsByTagName('span');
+        console.log(span);
+        for (let i = 0; i<span.length; i++) {
+            console.log(span[i])
+            span[i].classList.remove("form-valid");
+            span[i].classList.add("form-error")
+        }
+
            return alert("Please fill in all values."); 
         };
         if (!event.target.phone.value.match(validRegexPhone)) {
@@ -41,6 +57,8 @@ export default class AddWarehouseForm extends Component {
             this.props.history.push('/warehouses');
         })
     };
+
+         
     
     render(){
    
@@ -51,28 +69,36 @@ export default class AddWarehouseForm extends Component {
                 
                 <h2><NavLink to='/warehouses'><img className="add-warehouse__back-icon" src={backImg} alt="" /></ NavLink> Add New Warehouse</h2>
             </div>
-            <form onSubmit={this.handleSubmit} id='addWarehouse' className="add-warehouse__form-container">
+            <form onSubmit={this.handleSubmit} id='addWarehouse' className="add-warehouse__form-container" noValidate>
                 <div className="add-warehouse__form add-warehouse__form--left">
                     <h3 className="add-warehouse__subtitle">Warehouse Details</h3>
                     <label className="add-warehouse__label">Warehouse Name</label>
                     <input className="add-warehouse__input" required placeholder="Warehouse Name" name="name" id="name"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">Street Address</label>
                     <input className="add-warehouse__input" required placeholder="Street Address" name="address" id="address"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">City</label>
                     <input className="add-warehouse__input" required placeholder="City" name="city" id="city"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">Country</label>
                     <input className="add-warehouse__input" required placeholder="Country" name="country" id="country"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                 </div>
                 <div className="add-warehouse__form">
                     <h3 className="add-warehouse__subtitle">Contact Details</h3>
                     <label className="add-warehouse__label">Contact Name</label>
                     <input className="add-warehouse__input" required placeholder="Contact Name" name="contactName" id="contactName"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">Position</label>
                     <input className="add-warehouse__input" required placeholder="Position" name="position" id="position"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">Phone Number</label>
                     <input className="add-warehouse__input" required placeholder="Phone Number" name="phone" id="phone" type="phone"></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                     <label className="add-warehouse__label">Email</label>
                     <input className="add-warehouse__input"  placeholder="Email" name="email" id="email"   ></input>
+                    <span className="form-valid"><img src={error} alt="error alert" className='exclamation'/>This field is required</span>
                 </div>
             </form>
             <div className="add-warehouse__button-container">
